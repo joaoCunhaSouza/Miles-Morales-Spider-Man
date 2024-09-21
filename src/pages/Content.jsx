ab6d3ff6cc3aabfa691928fd472c109ed221e9c2
joaoCunhaSouza <joaovictorps4slim@gmail.com>
2024-09-20 21:04:38 -0300

    reload

--- src/pages/Content.jsx
+++ src/pages/Content.jsx
@@ -10,26 +10,31 @@ function Content() {
 
 
 
-        <header className="primary-header">
-
-  
-              <img src={images.svg} alt="Photographer's insight" className="logo-png" />
-              
-
-              <nav className="primary-nav">
-                <ul className="list">
-                  <li><a href="#">Home</a></li>
-                  <li><a href="#">Story</a></li>
-                  <li><a href="#">Wallpapers</a></li>
-                  <li><a href="#">SpiderMan2</a></li>
-                </ul>
-              </nav>
-
-            </header>
-        
+      <header className="primary-header">
 
 
-           
+        <img src={images.svg} alt="Photographer's insight" className="logo-png" />
+
+
+        <nav className="primary-nav">
+          <ul className="list">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Story</a></li>
+            <li><a href="#">Wallpapers</a></li>
+            <li><a href="#">SpiderMan2</a></li>
+          </ul>
+        </nav>
+
+      </header>
+
+      <div className="background-container">
+        <img src={images.back} alt="Photographer's insight" className="background" />
+      </div>
+
+
+
+
+
     </>
   );
 }
