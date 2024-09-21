686e9a9e1514b33608b6adf6e730f031938e6d13
Your Name <you@example.com>
2024-09-21 12:24:10 -0300

    update

--- src/imgs/images.jsx
+++ src/imgs/images.jsx
@@ -9,12 +9,15 @@ import img6 from './L6.png'; // Importação direta da imagem
 import img7 from './L7.png'; // Importação direta da imagem
 import svg from './svg.svg'; // Importação direta da imagem
 import logo from './spidermans.png'; // Importação direta da imagem
+import transparent from './transparent.png';
+import miles from './miles.png';
+import mileslogos from './svgs.svg';
 
 import back from './background.jpg';
 import window from './window.svg';
 
 const images = {
-    imgOne, img1,img2,img3,img4,img5,img6,img7,svg,logo,back,window
+    imgOne, img1,img2,img3,img4,img5,img6,img7,svg,logo,back,window,transparent,miles,mileslogos
 };
 
 export default images;
--- src/pages/Content.jsx
+++ src/pages/Content.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../style/main-pages.css';
 import images from "../imgs/images";
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 
 function Content() {
@@ -18,7 +19,7 @@ function Content() {
 
         <nav className="primary-nav">
           <ul className="list">
-            <li><a href="#">Home</a></li>
+            <li><a href="#" class="active">Home</a></li>
             <li><a href="#">Story</a></li>
             <li><a href="#">Wallpapers</a></li>
             <li><a href="#">Spider-Man 2</a></li>
@@ -27,12 +28,55 @@ function Content() {
 
       </header>
 
-      <div className="background-container">
-        <img src={images.back} alt="Photographer's insight" className="background" />
+      <div className="aside">
+
+        <div className="invisible"></div>
+
+        <div className="redes">
+          <i className="fa-brands fa-facebook"></i>
+          <i className="fa-brands fa-youtube"></i>
+          <i className="fa-brands fa-twitter"></i>
+        </div>
+
+        <img src={images.mileslogos} alt="Spider-Man Logo" className='svgs' />
       </div>
 
 
 
+      <div className="background-logo">
+
+        <img src={images.logo} alt="Spider-Man Logo" className='logo' />
+        <p>
+          In Spider-Man 2 for PS5, Miles Morales returns with an updated suit featuring
+          sharper lines and glowing accents that reflect his growing powers. Players will
+          experience refined agility and venom abilities as he teams up with Peter Parker
+          to face new threats in a vibrant New York City.
+        </p>
+
+        <div className="container-link">
+          <div className="PRE-ORDER-NOW"><h1>PRE-ORDER NOW</h1></div>
+          <div className="WATCH-TRAILER"><h1>WATCH TRAILER</h1></div>
+        </div>
+
+      </div>
+
+
+      <div className="background-container">
+
+        <img src={images.back} alt="Photographer's insight" className="background" />
+        <img src={images.transparent} alt="Photographer's insight" className="transparent" />
+
+      </div>
+
+
+      <div className="background-windows">
+
+        <img src={images.window} alt="Photographer's insight" className="windows" />
+        <img src={images.miles} alt="Photographer's insight" className="miles" />
+
+
+
+      </div>
 
 
     </>
